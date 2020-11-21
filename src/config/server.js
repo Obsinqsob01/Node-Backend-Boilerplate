2418ce1dbb35059503b1a84e906e9cb15e7392c3
Luis Contreras Guerra <luis@mailvibes.io>
2020-11-21 14:59:52 -0600

    Adding cors

--- src/config/server.js
+++ src/config/server.js
@@ -5,6 +5,12 @@ const server = express()
 
 server.use(express.urlencoded({ extended: true }))
 server.use(express.json({ extended: true }))
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*')
+
+    next()
+})
 
 setRoutes(server)
 
