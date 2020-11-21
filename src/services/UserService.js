d8d127eabb611101158d94df98dd4a2ee0f6bc67
Luis Contreras Guerra <luis@mailvibes.io>
2020-11-21 01:26:51 -0600

    Create service to login an user an return jwt

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,5 +1,6 @@
 import Service from "./Service"
-import { HashPassword } from "../utils/HashPassword"
+import jwt from "jsonwebtoken"
+import { HashPassword, ComparePassword } from "../utils/HashPassword"
 
 class UserService extends Service {
     async insert(data) {
@@ -17,6 +18,53 @@ class UserService extends Service {
                 error: true,
                 statusCode: 500,
                 message: errors.toString() || "Not able to create User",
+                errors
+            }
+        }
+    }
+
+    async login(email, password) {
+        try {
+            let user = await this.model.findOne({
+                email
+            })
+
+            if (!user) {
+                return {
+                    error: true,
+                    statusCode: 404,
+                    errors: "Not able to login",
+                    message: "User not found"
+                }
+            }
+
+            if (await ComparePassword(password, user.password_hash)) {
+                const payload = {
+                    email,
+                }
+
+                const token = jwt.sign(payload, process.env.KEY, {
+                    expiresIn: 1440,
+                })
+                
+                return {
+                    token,
+                    error: false,
+                    statusCode: 202,
+                }
+            }
+
+            return {
+                error: true,
+                statusCode: 401,
+                message: "Password is not valid",
+                errors: "Not be able to login at this time"
+            }
+        } catch (errors) {
+            return {
+                error: true,
+                statusCode: 500,
+                message: errors.toString() || "Not be able to login at this time",
                 errors
             }
         }
