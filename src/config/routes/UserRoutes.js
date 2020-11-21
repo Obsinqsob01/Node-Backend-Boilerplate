22c014c697b0e48a23efe78486d2d4c5016ee06d
Luis Contreras Guerra <luis@mailvibes.io>
2020-11-21 01:49:29 -0600

    Delete extra space

--- src/config/routes/UserRoutes.js
+++ src/config/routes/UserRoutes.js
@@ -1,12 +1,13 @@
 import UserController from "../../controllers/UserController"
 import express from "express"
+import { verifyToken } from "../middlewares/verifyToken"
 
 const router = express.Router()
 
 router.get("", UserController.getAll)
 router.post("", UserController.insert)
 router.post("/login", UserController.login)
-router.put("/:id", UserController.update)
+router.put("/:id", verifyToken, UserController.update)
 router.delete("/:id", UserController.delete)
 
 export default router
